cd53265b5f0574490eedbe1ca29bcf28ff2eee83
xviizenn <xviizenn@gmail.com>
2025-03-11 15:05:46 +0800

    Create Xero-Bots.user.js

---
+++ Xero-Bots.user.js
@@ -0,0 +1,49 @@
+// ==UserScript==
+// @name         Xero-Bots Beta | .io Bots 2025
+// @namespace    https://xero.up.railway.app/
+// @description  Best bots for .io
+// @author       Tatsuya & Enes & Neobots-dev/Slxsher & xviizenn
+// @match        *://agar.cc/*
+// @match        *://happyagar.online/*
+// @match        *://oceanar.io/*
+// @match        *://aquar.io/*
+// @match        *://cellsbox.io/*
+// @match        *://inciagario.net/*
+// @match        *://agariott.com/*
+// @match        *://bubleroyal.com/*
+// @match        *://agarioio.com/*
+// @match        *://agario.onl/*
+// @match        *://agario.org.uk/*
+// @match        *://agariomoddedserver.com/*
+// @match        *://agariomodded.com/*
+// @match        *://agarioonline.org/*
+// @match        *://agar.boston/*
+// @version      1.0.0
+// @run-at       document-start
+// @grant        none
+// @license      MIT
+// @icon         https://xero.up.railway.app/Xero.png
+// @require      https://xero.up.railway.app/Xero-Bots-Handler.js
+// @updateURL    https://xero.up.railway.app/Xero-Bots.user.js
+// @downloadURL  https://xero.up.railway.app/Xero-Bots.user.js
+// ==/UserScript==
+
+/*
+   The MIT License (MIT)
+   Copyright (c) 2025 Xero
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
