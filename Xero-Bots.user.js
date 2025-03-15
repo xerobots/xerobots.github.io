02691df7938d0c41cd7916fe619a49949ff9463e
xviizenn <xviizenn@gmail.com>
2025-03-15 20:43:04 +0800

    Update Xero-Bots.user.js

--- Xero-Bots.user.js
+++ Xero-Bots.user.js
@@ -1,32 +1,32 @@
  // ==UserScript==
 // @name         Xero-Bots Beta | .io Bots 2025
 // @namespace    https://xerobots.github.io/
+// @version      14/03/2025
 // @description  Best bots for agar.io clones and more
 // @author       Tatsuya & Enes & Neobots-dev/Slxsher & xviizenn
-// @match        *://oceanar.io/*
-// @match        *://agar.cc/*
-// @match        *://happyagar.online/*
-// @match        *://aquar.io/*
-// @match        *://gota.io/*
-// @match        *://easyagario.icu/*
-// @match        *://agarprivateservers.org/*
-// @match        *://cellsbox.io/*
-// @match        *://inciagario.net/*
-// @match        *://agariott.com/*
-// @match        *://bubleroyal.com/*
-// @match        *://agarioio.com/*
-// @match        *://agario.onl/*
-// @match        *://agario.org.uk/*
-// @match        *://agariomoddedserver.com/*
-// @match        *://agariomodded.com/*
-// @match        *://agarioonline.org/*
-// @match        *://agar.boston/*
-// @version      14/03/2025
-// @run-at       document-start
+// @match        *.oceanar.io/*
+// @match        *.agar.cc/*
+// @match        *.happyagar.online/*
+// @match        *.aquar.io/*
+// @match        *.gota.io/web/*
+// @match        *.easyagario.icu/*
+// @match        *.agarprivateservers.org/*
+// @match        *.cellsbox.io/*
+// @match        *.inciagario.net/*
+// @match        *.agariott.com/*
+// @match        *.bubleroyal.com/*
+// @match        *.agarioio.com/*
+// @match        *.agario.onl/*
+// @match        *.agario.org.uk/*
+// @match        *.agariomoddedserver.com/*
+// @match        *.agariomodded.com/*
+// @match        *.agarioonline.org/*
+// @match        *.agar.boston/*
 // @grant        none
-// @license      MIT
 // @icon         https://xerobots.github.io/Xero.png
 // @require      https://xerobots.github.io/Client.js
+// @license      MIT
+// @run-at       document-start
 // @updateURL    https://xerobots.github.io/Xero-Bots.user.js
 // @downloadURL  https://xerobots.github.io/Xero-Bots.user.js
 // ==/UserScript==
